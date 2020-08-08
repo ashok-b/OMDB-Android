eb1e229a876465b42186abfce8ca5439c970ba32
Ashok B <bashok.skk@gmail.com>
2020-08-09 00:24:14 +0530

    add application class update manifest file

---
+++ app/src/main/java/com/ashok/omdbsearch/OMDBApp.kt
@@ -0,0 +1,20 @@
+package com.ashok.omdbsearch
+
+import android.app.Application
+import com.ashok.academy.omdb.db.OMDbDatabase
+
+class OMDBApp : Application() {
+
+    companion object{
+        private lateinit var omDbDatabase: OMDbDatabase
+
+        fun imdbDatabase(): OMDbDatabase {
+            return omDbDatabase
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        omDbDatabase = OMDbDatabase.getDatabase(this);
+    }
+}
